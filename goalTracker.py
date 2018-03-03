a374cefc9f8b840d0f1f9cebc655913a5b3f53a4
da crands <davecrands@gmail.com>
2018-03-03 15:12:54 -0500

    Can now create new goals

--- goalTracker.py
+++ goalTracker.py
@@ -9,8 +9,7 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 from datetime import date
 
-
-# 
+#TODO prompt user to create goals
 try:
     wb = load_workbook(filename = "goal-tracker.xlsx")
 except IOError:
@@ -20,15 +19,32 @@ except IOError:
     # Create sheet names (meditate, walk, read)
     walkSheet = wb.active
     walkSheet.title = "walk"
-    walkSheet.cell(row=1, column=1).value  = 0
+    walkSheet.cell(row=1, column=1).value  = "DATE"
+    walkSheet.cell(row=1, column=2).value  = "MINUTES"
                   
     medSheet = wb.create_sheet(title="meditate")
-    medSheet.cell(row=1, column=1).value  = 0
+    medSheet.cell(row=1, column=1).value  = "DATE"
+    medSheet.cell(row=1, column=2).value  = "MINUTES"
                  
     readSheet = wb.create_sheet(title="read")
-    readSheet.cell(row=1, column=1).value = 0
+    readSheet.cell(row=1, column=1).value = "DATE"
+    readSheet.cell(row=1, column=2).value = "MINUTES"
                   
     wb.save("goal-tracker.xlsx")
+    
+def newSheet():
+    leaveNew = False
+    print("What activity would you like to add:")
+    name = input(">>> ")
+    while not leaveNew:
+        try:
+            newSheet = wb.create_sheet(name)
+            newSheet.cell(row=1, column=1).value  = "DATE"
+            newSheet.cell(row=1, column=2).value  = "MINUTES"
+            leaveNew = True
+        except ValueError:
+            print("invalid");
+
     
 
 def chooseSheet():
@@ -52,16 +68,19 @@ def chooseSheet():
 
 leave = False
 while not leave:
-    print("Press 'n' to update goal. Press 'q' to quit")
-    choices = ['q', 'n']
+    print("Press 'n' to update goal.\nPress 'a' to add a goal.\nPress 'q' to quit")
+    choices = ['q', 'n', 'a']
     choice = input(">>>")
         
     try:
         if choice not in choices:
             raise ValueError()
             
-        if choice == 'q':
+        elif choice == 'q':
             leave = True
+            
+        elif choice == 'a':
+            newSheet()
             
         elif choice == 'n':
             sheetLeave = False
@@ -76,7 +95,8 @@ while not leave:
                     for rowNum in range(1, sheet.max_row+2):
                         if sheet.cell(row=rowNum, column=1).value is None:
                             sheet.cell(row=rowNum, column=1).value = str(date.today())
-                            sheet.cell(row=rowNum, column=2).value = time    
+                            sheet.cell(row=rowNum, column=2).value = time
+                            print("You successfully updated {0}".format(currSheet))
                             sheetLeave = True
                             
                 except ValueError:
