3b9463d973ef018218a50efd97c0c1976d4212c4
da crands <davecrands@gmail.com>
2018-03-03 13:27:04 -0500

    Increased exception handling, no more 'breaks'

--- goalTracker.py
+++ goalTracker.py
@@ -5,62 +5,87 @@ Created on Sat Apr 15 16:19:20 2017
 
 @author: dacrands
 """
-#from openpyxl import Workbook
+from openpyxl import Workbook
 from openpyxl import load_workbook
 from datetime import date
 
 
-## Create the wb (uncomment to create)
-#wb = Workbook()
-## Create sheet names (meditate, walk, read)
-#walkSheet = wb.active
-#walkSheet.title = "walk"
-#
-#medSheet = wb.create_sheet(title="meditate")
-#
-#readSheet = wb.create_sheet(title="read")
-#
-#wb.save("goal-tracker.xlsx")
+# 
+try:
+    wb = load_workbook(filename = "goal-tracker.xlsx")
+except IOError:
+    print("Creating workbook")
+    wb = Workbook()
+    
+    # Create sheet names (meditate, walk, read)
+    walkSheet = wb.active
+    walkSheet.title = "walk"
+    walkSheet.cell(row=1, column=1).value  = 0
+                  
+    medSheet = wb.create_sheet(title="meditate")
+    medSheet.cell(row=1, column=1).value  = 0
+                 
+    readSheet = wb.create_sheet(title="read")
+    readSheet.cell(row=1, column=1).value = 0
+                  
+    wb.save("goal-tracker.xlsx")
+    
 
-# Load the wb
-wb = load_workbook(filename = "goal-tracker.xlsx")
-
-# 1. Prompt user for activity (perhaps: "Are you sure?")
-def chooseColor():
-    while True:
+def chooseSheet():
+    leaveSheet = False
+    while not leaveSheet:
         print("Choose an activity: ")
         for i in range(len(wb.sheetnames)):
             print("{0}: {1}\r".format(i, wb.sheetnames[i]))
-        sheetChoice = input(">>>")
+            
+        sheetChoice = input(">>>")        
         try:
             sheetChoice = int(sheetChoice)
             currSheet = wb.sheetnames[sheetChoice]
             print(currSheet)
-            return currSheet
-            break
+            leaveSheet = True
+            return currSheet 
+           
         except:
             print("That's not a valid option")
 
-currSheet = chooseColor()
-sheet = wb[currSheet]   
-print(sheet.max_row)
-# 2. Open corresponding sheet, prompt for data (perhaps: "Is the data accurate?")
 
-while True:
-    time = input("How long were you doing said task? >>")
-    try: 
-        int(time)
-        break
-    except ValueError:
-        print("Please enter an integer, representing minutes...REPRESENT, B!")
+leave = False
+while not leave:
+    print("Press 'n' to update goal. Press 'q' to quit")
+    choices = ['q', 'n']
+    choice = input(">>>")
         
-for rowNum in range(1, sheet.max_row+2):
-    if sheet.cell(row=rowNum, column=1).value is None:
-        sheet.cell(row=rowNum, column=1).value = str(date.today())
-        sheet.cell(row=rowNum, column=2).value = time
+    try:
+        if choice not in choices:
+            raise ValueError()
+            
+        if choice == 'q':
+            leave = True
+            
+        elif choice == 'n':
+            sheetLeave = False
+            while not sheetLeave:
+                currSheet = chooseSheet()
+                sheet = wb[currSheet]   
+                print(sheet.max_row)
+                time = input("How long were you doing said task? >>")
+                
+                try: 
+                    int(time)
+                    for rowNum in range(1, sheet.max_row+2):
+                        if sheet.cell(row=rowNum, column=1).value is None:
+                            sheet.cell(row=rowNum, column=1).value = str(date.today())
+                            sheet.cell(row=rowNum, column=2).value = time    
+                            sheetLeave = True
+                            
+                except ValueError:
+                    print("Please enter an integer, representing minutes...REPRESENT, B!")
+                
+    except ValueError:
+        print("That's not a valid option")
 
-   
 
 wb.save("goal-tracker.xlsx")
-# 3. "Anything else" (if yes, go back to step one)
+
 
